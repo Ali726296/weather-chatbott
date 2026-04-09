8b59ba7d9eefd7606f7721811c7e87fd5364fa3d
Ali726296 <mahamadshaikh3768@gmail.com>
2026-04-09 23:01:52 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,3 @@
-from flask import Flask, render_template
-import os
-
-template_dir = os.path.join(os.path.dirname(__file__), "../templates")
-
-app = Flask(__name__, template_folder=template_dir)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
+from app import app
 
 handler = app
